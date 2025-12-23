279b4d496ccc7857ace5f1b0a7e4b75e35e0f022
yupr <12003186+yupr@users.noreply.github.com>
2025-12-24 01:45:34 +0900

    不要なリソースを削除

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 import { PostModule } from './post/post.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CatsModule, PrismaModule, PostModule],
+  imports: [PrismaModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
